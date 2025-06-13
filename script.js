916194cc1740fc5ae229d8c6004468ff7cabdbbf
ataftan <134144780+ataftan@users.noreply.github.com>
2025-06-12 20:21:09 -0400

    changes 12/6
    updated with map image and added spiraltime html

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
- $('.husk').each(function () {
-      const $container = $(this);
-      const $touched = $container.find('.touched');
-      const $burst = $container.find('.burst');
+$('.husk').each(function () {
+  const $container = $(this);
+  const $touched = $container.find('.touched');
+  const $burst = $container.find('.burst');
+  const offsetX = parseInt($container.data('offset-x')) || 0;
+  const offsetY = parseInt($container.data('offset-y')) || 0;
 
-      $touched.on('mouseenter', function () {
-        $burst.css('opacity', 1);
-      });
+  $touched.on('mouseenter', function () {
+    $burst.css('opacity', 1);
+  });
 
-      $touched.on('mousemove', function (e) {
-        const offsetX = 3;
-        const offsetY = 3;
-        $burst.css({
-          left: e.clientX + offsetX + 'px',
-          top: e.clientY + offsetY + 'px'
-        });
-      });
-
-      $touched.on('mouseleave', function () {
-        $burst.css('opacity', 0);
-      });
+  $touched.on('mousemove', function (e) {
+    $burst.css({
+      left: e.clientX + offsetX + 'px',
+      top: e.clientY + offsetY + 'px'
     });
+  });
 
-$(".i").on("click", function() {
+  $touched.on('mouseleave', function () {
+    $burst.css('opacity', 0);
+  });
+});
+
+$(".i").on("click", function () {
   const $txt = $("#txt");
   if ($txt.is(":visible")) {
     $txt.hide();
@@ -35,3 +35,17 @@ $(".i").on("click", function() {
 
 
 
+
+$("body").on("click", function (e) {
+
+  if ($(e.target).closest(".hypertext").length > 0) {
+    return; // do nothing if clicked inside about button
+  }
+  $(this).toggleClass("toggled");
+
+  if ($(this).hasClass("toggled")) {
+    $('#pre-txt').fadeIn();
+  } else {
+    $('#pre-txt').fadeOut();
+  }
+});
